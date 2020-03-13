44b6eb60277c7a1d3ac3da6d3229d87bd5cd065d
MARCO ANTONIO BARAO NEVES <marco.abneves@senacsp.edu.br>
2020-03-13 20:08:45 -0300

    rename

--- Azul/Azul.Designer.cs
+++ Azul/Azul.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Azul
 {
-    partial class Form1
+    partial class Azul
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Azul
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Azul";
+            this.Text = "Azul";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Azul/Azul.cs
+++ Azul/Azul.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Azul
 {
-    public partial class Form1 : Form
+    public partial class Azul : Form
     {
-        public Form1()
+        public Azul()
         {
             InitializeComponent();
         }
